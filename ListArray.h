4ff8379fd63804569914dc09ffb2627dcab115ce
GARNESP <garnesp@alumno.upv.es@ldsic-vdi05.upvnet.upv.es>
2024-09-24 20:14:11 +0200

    la segunda commit

---
+++ ListArray.h
@@ -0,0 +1,18 @@
+#include <ostream>
+#include "List.h"
+
+template <typename T> 
+class ListArray : public List<T> {
+
+    private:
+        // miembros privados
+	T* arr;
+	int max;
+	int n;
+	static const int MINSIZE;
+
+    public:
+        // miembros públicos, incluidos los heredados de List<T>
+    
+};
+~            
